7c2c3411449b0ebf3e667e80e5488e31e9ac710c
Nicole Dow <nikki.dow71@gmail.com>
2019-12-11 15:05:34 -0700

    add handleOnChange for form inputs

--- coverless-frontend/src/Components/Login.js
+++ coverless-frontend/src/Components/Login.js
@@ -6,11 +6,18 @@ export default class Login extends React.Component {
         password: ''
     }
 
+    handleOnChange = () => {
+        this.setState({
+            email: document.getElementById('email').value,
+            password: document.getElementById('password').value
+        })
+    }
+
     render() {
         return (
             <form>
-                <input type="text" placeholder="Email Address" value={this.state.email} />
-                <input type="password" placeholder="Password" value={this.state.password} />
+                <input id="email" type="text" placeholder="Email Address" value={this.state.email} onChange={this.handleOnChange}/>
+                <input id="password" type="password" placeholder="Password" value={this.state.password} onChange={this.handleOnChange}/>
                 <input type="submit" value="Log In" />
             </form>
         )

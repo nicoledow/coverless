e9c6708d9702c83fdc9b0f40a8d6686e51d8d770
Nicole Dow <nikki.dow71@gmail.com>
2019-12-11 12:41:13 -0700

    generate models and db schema

---
+++ coverless-api/db/migrate/20191211193524_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.integer :author_id
+      t.text :blurb
+
+      t.timestamps
+    end
+  end
+end

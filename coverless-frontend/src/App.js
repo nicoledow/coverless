35f0b7a7581afcd096b59fbb4da16009abef43bf
Nicole Dow <nikki.dow71@gmail.com>
2019-12-17 11:24:48 -0500

    move header into its own component

--- coverless-frontend/src/App.js
+++ coverless-frontend/src/App.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
-/*import { BrowserRouter as Router, Route, Link  } from 'react-router-dom';
-import SignUp from './Components/Signup';
-import Login from './Components/Login';*/
 import BookContainer from './Components/BookContainer';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      {/*<Router>*/}
+    
         <Navbar />
     
-        <h1>Coverless</h1>
-        <h3>Don't pick a book by its cover.</h3>
+        <Header />
 
         <BookContainer />
 
-        {/*<Link to="/users/new"><button>Sign Up</button></Link>
-        <Link to="/login"><button>Log In</button></Link>
-        <Route exact path="/users/new" component={SignUp} />
-        <Route exact path="/login" component={Login} />
-  </Router>*/}
     </div>
   );
 }
---
+++ coverless-frontend/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div id="header-div">
+          <h1>Coverless</h1>
+          <h2>Don't pick a book by its cover.</h2>
+        </div>
+    )
+}
+
+export default Header;

ca0ab8a4d972aff1c9fdbb1475a9d0ae515fc21c
Nicole Dow <nikki.dow71@gmail.com>
2019-12-23 18:38:41 -0500

    remove console logs

--- coverless-frontend/src/reducers/handlebooks.js
+++ coverless-frontend/src/reducers/handlebooks.js
@@ -7,12 +7,10 @@ const handleBooks = (state = { books: [], lists: [], currentList: '', likedBooks
         case 'START_FETCHING_LISTS':
             return {...state, requesting: true }
         case 'START_FETCHING_LIKED_BOOKS_REQUEST':
-            console.log('starting to fetch liked books')
             return {...state, likedBooks: [...state.likedBooks], requesting: true }
         case 'ADD_LISTS':
             return {...state, requesting: false, lists: action.listData }
         case 'ADD_BOOKS':
-            console.log('adding books in reducer', action)
             return {...state, books: [action.books], requesting: false }
         case 'START_ADDING_LIKED_BOOK':
             return {...state, requesting: true}
@@ -20,13 +18,10 @@ const handleBooks = (state = { books: [], lists: [], currentList: '', likedBooks
             let likedBook = state.books.find(b => b.primary_isbn10 === action.book.isbn) //is this unneccessary? Backend is handling this?
             return {...state}
         case 'ADD_LIKED_BOOKS':
-            console.log('in reducer adding liked books data')
             return {...state, likedBooks: action.likedBooks, requesting: false }
         case 'START_REMOVE_LIKED_BOOK_REQ':
             return {...state, requesting: true }
         case 'REMOVE_FROM_LIKED_BOOKS':
-            console.log('hit reducer for removing book', action.isbn)
-            console.log(state.books)
             let likedBooks = state.books.filter(b => b.isbn !== action.isbn)
             return {...state, books: likedBooks, requesting: false }
         case 'ADD_USER':

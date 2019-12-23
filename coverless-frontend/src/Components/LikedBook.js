bb12c7dc23ef96425722a606376f2577593b1048
Nicole Dow <nikki.dow71@gmail.com>
2019-12-23 09:56:58 -0500

    add X button to likedBook for removal

--- coverless-frontend/src/Components/LikedBook.js
+++ coverless-frontend/src/Components/LikedBook.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import RemoveButton from './RemoveButton';
 
 export default class LikedBook extends React.Component {
 
     render() {
-        return <p>LikedBook</p>
+        console.log('liked book comp', this.props)
+        let book = this.props.data;
+        return (
+            <div className="card">
+                <div className="card-header">{book.title}</div>
+                <div className="card-body">
+                    {book.blurb}
+                </div>
+                <RemoveButton isbn={book.isbn}/>
+            </div>
+        )
     }
 }

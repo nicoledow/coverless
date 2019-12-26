f6d856b3306261421a0bbec676274bee5a260b59
Nicole Dow <nikki.dow71@gmail.com>
2019-12-26 11:38:31 -0500

    remove unused case statements and reorganize order

--- coverless-frontend/src/reducers/handlebooks.js
+++ coverless-frontend/src/reducers/handlebooks.js
@@ -1,11 +1,11 @@
 const handleBooks = (state = { books: [], lists: [], currentList: '', likedBooks: [], requesting: false }, action) => {
     switch(action.type){
-        case 'START_FETCHING_NEW_USER_REQ':
-            return {...state, requesting: true }
         case 'START_ADDING_BOOKS_REQ':
             return {...state, books: [...state.books], currentList: action.listName, requesting: true}
         case 'START_FETCHING_LISTS':
             return {...state, requesting: true }
+        case 'UPDATE_CURRENT_LIST':
+            return {...state, currentList: action.listName }
         case 'START_FETCHING_LIKED_BOOKS_REQUEST':
             return {...state, likedBooks: [...state.likedBooks], requesting: true }
         case 'ADD_LISTS':
@@ -14,9 +14,6 @@ const handleBooks = (state = { books: [], lists: [], currentList: '', likedBooks
             return {...state, books: [action.books], requesting: false }
         case 'START_ADDING_LIKED_BOOK':
             return {...state, requesting: true}
-        case 'ADD_LIKED_BOOK':
-            let likedBook = state.books.find(b => b.primary_isbn10 === action.book.isbn) //is this unneccessary? Backend is handling this?
-            return {...state}
         case 'ADD_LIKED_BOOKS':
             return {...state, likedBooks: action.likedBooks, requesting: false }
         case 'START_REMOVE_LIKED_BOOK_REQ':
@@ -24,10 +21,6 @@ const handleBooks = (state = { books: [], lists: [], currentList: '', likedBooks
         case 'REMOVE_FROM_LIKED_BOOKS':
             let filteredLikedBooks = state.likedBooks.filter(b => b.isbn !== action.isbn)
             return {...state, likedBooks: filteredLikedBooks, requesting: false }
-        case 'ADD_USER':
-            return {...state, currentUser: action.user }
-        case 'UPDATE_CURRENT_LIST':
-            return {...state, currentList: action.listName }
         default:
             return state;
     }

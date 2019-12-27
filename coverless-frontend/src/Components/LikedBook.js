edad3df19a90d2505a634b16113537b6af91d0a3
Nicole Dow <nikki.dow71@gmail.com>
2019-12-27 10:54:01 -0500

    add no-referrer to a tag

--- coverless-frontend/src/Components/LikedBook.js
+++ coverless-frontend/src/Components/LikedBook.js
@@ -18,7 +18,7 @@ export default class LikedBook extends React.Component {
                 </div>
                 <div className="card-body">
                     <p>{book.blurb}</p>
-                    <button className="btn btn-info"><a href={book_url} target="_blank">Buy on Amazon</a></button>
+                    <button className="btn btn-info"><a href={book_url} target="_blank" rel="noopener noreferrer">Buy on Amazon</a></button>
                 </div>
             </div>
         )
